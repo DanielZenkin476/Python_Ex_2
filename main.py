594c8e143cfdfcd296a2c5ae1ad802e2467b945a
Daniel Zenkin <Danielzenkin82@gmail.com>
2024-11-03 10:02:08 +0200

    works, runtime 2 msec(beats 100) memory 11.65MB(beats 32.79)

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ class Solution(object):
         """
         return bisect_right(range(x+1),0,key = lambda q:q*q-x)-1
 
-
     def mySqrt_2(self,x):
         if x == 0:
             return 0
@@ -30,6 +29,29 @@ class Solution(object):
             if mult>x:
                 right = mid-1
         return right
+
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n<=1:# only one element or none
+            return
+        if n==2:# [x,y] -> [y,x]
+            nums.reverse()
+            return
+        i = n-2
+        while(i>=0 and nums[i]>=nums[i+1]):# check if aray is in ascending order -if so reverse
+            i-=1
+        if i ==-1:
+            nums.reverse()
+            return
+        j = n-1
+        while nums[j]<=nums[i]:
+            j-=1
+        nums[i],nums[j] = nums[j],nums[i]
+        nums[i+1:] = reversed(nums[i+1:])
 
 
 sol = Solution()
