2788b6c3bb33badd24b518aad9d511c6f2f5f049
Daniel Zenkin <Danielzenkin82@gmail.com>
2024-11-04 10:45:57 +0200

    done with search, runtime 0msec, memory 11.67, beats 94%

--- main.py
+++ main.py
@@ -139,6 +139,31 @@ class Solution(object):
                     max_len = max(max_len,i-char_lst[-1])
         return max_len
 
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        while (left<=right):
+            mid = (left+right)//2
+            if nums[mid]==target:
+                return mid
+            if nums[left]<= nums[mid]:
+                if nums[left]<= target < nums[mid]:
+                    right = mid-1
+                else :
+                    left = mid+1
+            else:
+                if nums[mid]<target<= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid-1
+        return -1
+
+
 
 
 
@@ -146,4 +171,4 @@ class Solution(object):
 
 
 sol = Solution()
-print(sol.longestValidParentheses_3('()(())'))
+print(sol.search(nums = [4,5,6,7,0,1,2], target = 6))
