0cc0d2e38fb2dad0afe17dca97452853edf90bcc
Daniel Zenkin <Danielzenkin82@gmail.com>
2024-11-05 14:51:03 +0200

    trying with recursion - c(n) = c(n-1) = c(n-2)

--- main.py
+++ main.py
@@ -257,6 +257,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        if n ==1:
+            return 1
+        elif n==2 : return 2
+        else: return self.climbStairs(n-1)+self.climbStairs(n-2)
+
 
 
 
